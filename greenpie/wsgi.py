25b222e10eada900101f1302bccf079f90d97003
Dmitrii Kudriakov <kudriakovda@gmail.com>
2025-04-14 23:40:28 -0400

    Vercel commit v1.2

--- greenpie/wsgi.py
+++ greenpie/wsgi.py
@@ -2,6 +2,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
+WSGI_APPLICATION = 'api.wsgi.app'
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greenpie.settings')
 
 application = get_wsgi_application()
